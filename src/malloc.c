54d4a5f05650b04f426d374b3879b67a8320ce19
collin_b <collin_b@epitech.eu>
2014-01-31 12:00:58 +0100

    just useless modifications

--- src/malloc.c
+++ src/malloc.c
@@ -5,19 +5,27 @@
 ** Login   <collin_b@epitech.net>
 ** 
 ** Started on  Fri Jan 31 11:45:34 2014 jonathan.collinet
-** Last update Fri Jan 31 11:51:42 2014 jonathan.collinet
+** Last update Fri Jan 31 12:00:02 2014 jonathan.collinet
 */
 
 #include "malloc.h"
 
 void		*malloc(size_t size)
 {
+  if (!size)
+    printf("yollolollol!\n");
+  return ((void*)0x0);
 }
 
 void		*realloc(void *ptr, size_t size)
 {
+  if (!ptr && !size)
+    printf("yolo!\n");
+  return (ptr);
 }
 
 void		free(void *ptr)
 {
+  if (!ptr)
+    return ;
 }
