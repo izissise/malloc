376070e05d6a829742af8e7a51081172bbb83d6d
collin_b <collin_b@epitech.eu>
2014-02-03 21:13:07 +0100

    delete debug in search_node

--- src/search_node.c
+++ src/search_node.c
@@ -5,7 +5,7 @@
 ** Login   <moriss_h@epitech.net>
 **
 ** Started on  Mon Oct  8 09:34:29 2012 hugues morisset
-** Last update Mon Oct  8 16:20:21 2012 hugues morisset
+** Last update Mon Feb  3 21:12:39 2014 jonathan.collinet
 */
 
 #include "malloc.h"
@@ -17,7 +17,6 @@ t_list	*find_free_size_node(t_list *last_node, size_t req_size)
   tmp_node = last_node;
   while (tmp_node)
     {
-      printf("node: %p free: %u size: %u asize: %u\n", tmp_node, tmp_node->is_free, NODESIZE(tmp_node), req_size);
       if (tmp_node->is_free
           && (NODESIZE(tmp_node) >= req_size))
         {
