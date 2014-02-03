07959dc5d98ad0e260405826fd64aad5dabba608
izissise <hugues.m@hotmail.fr>
2014-02-03 21:00:57 +0100

    Add dump

--- test/main.c
+++ test/main.c
@@ -1,18 +1,26 @@
 /*
 ** main.c for main in /home/collin_b/project/malloc/PSU_2013_malloc/debug
-** 
+**
 ** Made by jonathan.collinet
 ** Login   <collin_b@epitech.net>
-** 
+**
 ** Started on  Mon Feb  3 11:18:40 2014 jonathan.collinet
-** Last update Mon Feb  3 18:03:09 2014 jonathan.collinet
+** Last update Mon Feb  3 18:31:36 2014 Hugues
 */
 
 #include "../src/malloc.h"
 
+void hexdump(void *mem, unsigned int len);
+
 int			main()
 {
+void* ptr = sbrk(0);
   show_alloc_mem();
-  malloc(8121);
+  malloc(4025);
   show_alloc_mem();
+  malloc(1);
+  show_alloc_mem();
+void *end = sbrk(0);
+hexdump(ptr, end - ptr);
+  return 0;
 }
